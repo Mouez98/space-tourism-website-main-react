d1a4efa499f80fb59560e6b77c5751f97da85627
Mouez98 <mouez.matoui@gmail.com>
2022-04-29 02:14:41 +0100

    edits

--- src/pages/Technology.js
+++ src/pages/Technology.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { useLocation } from "react-router-dom";
 
+import '../components/technology/Technology.css'
 
 const Technology = ({technology}) => {
   const [curTechnology, setCurTechnology ] = useState(0)
@@ -13,17 +14,20 @@ const Technology = ({technology}) => {
 
   return (
     <section className={cssClass}>
-      <h2>
+      <h2 className="title">
         <span>02</span> Meet your crew
       </h2>
       <article>
-        <img src={images.portrait} alt={name} />
+        <div className="image-container">
+
+        <img src={images.landscape} alt={name} />
+        </div>
         <div className="details">
-          <ul>
+          <ul className="technology-list">
           {technology ? technology.map((des, index )=> <li key={des.name} onClick={()=>setCurTechnology(index)}>{index + 1}</li>): null }
           </ul>
           <h3 className="header">The terminolgy</h3>
-          <h3 className="header">{name}</h3>
+          <h3 className="sub-header">{name}</h3>
           <p className="description">
           {description}
           </p>
